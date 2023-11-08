f9df09cd61241f509ac8ef09d61e110400ead123
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2023-11-08 19:37:22 +0000

    frase aleatoria

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let newArray = [];
+  for (let i = 0; i < 4; i++) {
+    if (i === 2) {
+      newArray[i] = Math.floor(Math.random() * 3);
+    } else if (i === 3) {
+      newArray[i] = Math.floor(Math.random() * 5);
+    } else {
+      newArray[i] = Math.floor(Math.random() * 4);
+    }
+  }
+  let frase =
+    who[newArray[0]] +
+    " " +
+    action[newArray[1]] +
+    " " +
+    what[newArray[2]] +
+    " " +
+    when[newArray[3]];
+
+  document.getElementById("excuse").innerHTML = frase;
 };
